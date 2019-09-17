b5ddb93f7240166acf45ab7bb8de99f928db746e
Sarthak Chowdhary <sarthak.chow@gmail.com>
2019-09-17 19:32:53 +0530

    feat - created React Components Header SignUp/SignIn Modal Timeline HomePage fixes #5 Resolved Github comments

---
+++ client/src/components/molecules/EmailAndPassInput/EmailAndPassInput.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Form } from 'react-bootstrap';
+
+const EmailAndPassInput = () => {
+  return (
+    <>
+      <Form.Group controlId='formBasicEmail'>
+        <Form.Label hidden>Email address</Form.Label>
+        <Form.Control type='email' placeholder='Enter email' />
+        <Form.Text className='text-muted'>
+          We'll never share your email with anyone else.
+        </Form.Text>
+      </Form.Group>
+      <Form.Group controlId='formBasicPassword'>
+        <Form.Label hidden>Password</Form.Label>
+        <Form.Control type='password' placeholder='Password' />
+      </Form.Group>
+    </>
+  );
+};
+
+export default EmailAndPassInput;
