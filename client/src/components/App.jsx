2a1f870ac22fc7f3152c8b79e7ba3d08ab689276
Sarthak Chowdhary <sarthak.chow@gmail.com>
2019-09-19 18:44:58 +0530

    feat: added routes
    / (home)
    /timeline

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import Header from './organisms/Header/Header';
-import TripList from './organisms/TripList/TripList';
+import HomePage from './pages/HomePage/HomePage';
+import TimeLine from './pages/TimeLine/TimeLine';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
 
 export default function App() {
   return (
     <div>
-      <Header></Header>
-      <TripList></TripList>
+      <BrowserRouter>
+        <Header></Header>
+        <Route path="/" exact component={HomePage}></Route>
+        <Route path="/timeline" exact component={TimeLine}></Route>
+      </BrowserRouter>
     </div>
   );
 }
