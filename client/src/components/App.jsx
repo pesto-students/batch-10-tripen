49c8f4428cdd837470589c86456f826b3f6e565f
Sarthak Chowdhary <sarthak.chow@gmail.com>
2019-09-20 21:57:44 +0530

    Refactored App.jsx

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,27 +1,33 @@
-import React from "react";
-import Header from "./organisms/Header/Header";
-import HomePage from "./pages/HomePage/HomePage";
-import TimeLinePage from "./pages/TimeLinePage/TimeLinePage";
-import { Switch, Route } from "react-router-dom";
-import PrivateRoute from "./organisms/PrivateRoute/PrivateRoute";
+import React from 'react';
+import Header from './organisms/Header/Header';
+import HomePage from './pages/HomePage/HomePage';
+import TimeLinePage from './pages/TimeLinePage/TimeLinePage';
+import { Switch, Route } from 'react-router-dom';
+import PrivateRoute from './organisms/PrivateRoute/PrivateRoute';
 
 export default function App() {
   return (
     <div>
       <Header />
       <Switch>
-        <Route path='/' exact component={HomePage} />
-        <Route path='/sign-in' exact render={props => <HomePage {...props} modalState={true} />} />
+        <Route path="/" exact component={HomePage} />
         <Route
-          path='/sign-up'
+          path="/sign-in"
           exact
-          render={props => <HomePage {...props} modalState={true} signUp={true} />}
+          render={(props) => <HomePage {...props} modalState={true} />}
         />
-        <Route path='/timeline' component={TimeLinePage} />
-        <Route path='/trip' exact component={TimeLinePage} />
-        <PrivateRoute path='/profile' exact component={TimeLinePage} />
-        <PrivateRoute path='/edit-timeline' component={TimeLinePage} />
-        <PrivateRoute path='/create-timeline' component={TimeLinePage} />
+        <Route
+          path="/sign-up"
+          exact
+          render={(props) => (
+            <HomePage {...props} modalState={true} signUp={true} />
+          )}
+        />
+        <Route path="/timeline" component={TimeLinePage} />
+        <Route path="/trip" exact component={TimeLinePage} />
+        <PrivateRoute path="/profile" exact component={TimeLinePage} />
+        <PrivateRoute path="/edit-timeline" component={TimeLinePage} />
+        <PrivateRoute path="/create-timeline" component={TimeLinePage} />
         <Route component={HomePage} />
       </Switch>
     </div>
