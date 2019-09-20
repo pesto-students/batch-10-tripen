bdae3e40dbe6a7575f08e6ef931cbe0f76385a06
Karan Sapolia <sapoliakaran@gmail.com>
2019-09-20 17:36:35 +0530

    add mlab database url

--- server/src/controllers/posts.js
+++ server/src/controllers/posts.js
@@ -1,5 +1,14 @@
-import 
+import { Router } from 'express';
+import mongoose from 'mongoose';
+import Post from '../models/post';
+import Timeline from '../models/timeline';
 
-const createPost = (req, res) => {
-  
-}
+const router = Router();
+
+router.post('/', (req, res) => {
+  try {
+    const timeline = await Timeline.findOne({ 
+      _id: req.body.id
+    })
+  }
+})
