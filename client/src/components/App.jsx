ec5d38449b1c3ef6a0d8ef8f51050736d5941140
p-racoon <prasham9.ash@gmail.com>
2019-09-20 17:17:52 +0530

    fix: routes with switch ids & default path

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,17 +1,22 @@
-import React from 'react';
-import Header from './organisms/Header/Header';
-import HomePage from './pages/HomePage/HomePage';
-import TimeLinePage from './pages/TimeLinePage/TimeLinePage';
-import { BrowserRouter, Route } from 'react-router-dom';
+import React from "react";
+import Header from "./organisms/Header/Header";
+import HomePage from "./pages/HomePage/HomePage";
+import TimeLinePage from "./pages/TimeLinePage/TimeLinePage";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 export default function App() {
   return (
     <div>
-      <Header></Header>
-      <BrowserRouter>
-        <Route path="/" exact component={HomePage}></Route>
-        <Route path="/timeline" exact component={TimeLinePage}></Route>
-      </BrowserRouter>
+      <Header />
+      <Router>
+        <Switch>
+          <Route path='/' exact component={HomePage}></Route>
+          <Route path='/timeline/:timelineid' exact component={TimeLinePage}></Route>
+          <Route path='/edit-timeline/:timelineid' exact component={TimeLinePage}></Route>
+          {/* <Route path='/profile/:userid' exact component={ProfilePage}></Route> */}
+          <Route component={HomePage} />
+        </Switch>
+      </Router>
     </div>
   );
 }
