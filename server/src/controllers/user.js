e4c9fca7c5821f3b57bd76740d0012dee5eaad7d
thapasya <thapasya.murali@gmail.com>
2019-09-20 18:06:32 +0530

    CORS fix

--- server/src/controllers/user.js
+++ server/src/controllers/user.js
@@ -14,16 +14,7 @@ router.get('/', async function(req, res) {
     res.status(200).send(response);
 });
 router.post('/', async function(req, res) {
-    const input = req.body;
-    const userObj = new userModel({
-        _id: mongoose.Types.ObjectId(),
-        name: input.lname,
-        username: input.username,
-        createdOn: new Date(),
-        email: input.emailId,
-        password: input.pwd
-    })
-    const response = await userObj.save();
+    const response = await userModel.create(req.body);
     res.status(200).send({data: response});
 });
 
