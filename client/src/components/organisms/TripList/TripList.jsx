cea8c621fde67ca6fe7c644261279bfc91fd625b
p-racoon <prasham9.ash@gmail.com>
2019-09-20 19:45:17 +0530

    routes Private Routes

--- client/src/components/organisms/TripList/TripList.jsx
+++ client/src/components/organisms/TripList/TripList.jsx
@@ -1,28 +1,30 @@
-import React from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
 
-import TripCard from '../../molecules/TripCard/TripCard';
+import TripCard from "../../molecules/TripCard/TripCard";
 
-export default function TripList() {
+const TripList = props => {
+  const timelineid = 123;
   return (
     <Container>
       <Row>
-        <Col md={3}>
+        <Col md={3} onClick={() => props.history.push(`timeline/${timelineid}`)}>
           <TripCard></TripCard>
         </Col>
-        <Col md={3}>
+        <Col md={3} onClick={() => props.history.push(`timeline/${timelineid}`)}>
           <TripCard></TripCard>
         </Col>
-        <Col md={3}>
+        <Col md={3} onClick={() => props.history.push(`timeline/${timelineid}`)}>
           <TripCard></TripCard>
         </Col>
-        <Col md={3}>
+        <Col md={3} onClick={() => props.history.push(`timeline/${timelineid}`)}>
           <TripCard></TripCard>
         </Col>
-        <Col md={3}>
+        <Col md={3} onClick={() => props.history.push(`timeline/${timelineid}`)}>
           <TripCard></TripCard>
         </Col>
       </Row>
     </Container>
   );
-}
+};
+export default TripList;
