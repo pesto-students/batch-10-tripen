4adb18be571d6549875b64f504ca9ef2860e6afe
Sarthak Chowdhary <sarthak.chow@gmail.com>
2019-09-26 20:34:23 +0530

    timeline card data types fixed

--- client/src/components/organisms/TimeLine/TimeLine.jsx
+++ client/src/components/organisms/TimeLine/TimeLine.jsx
@@ -6,11 +6,25 @@ import * as data from './TimeLine.json';
 
 const TimeLine = () => {
   const cards = data.default.posts;
+  console.log(cards);
   const timeLineCards = cards.map((card) => {
+    const dateOptions = {
+      weekday: 'short',
+      year: 'numeric',
+      month: 'short',
+      day: 'numeric',
+    };
+    const timeOptions = {
+      hour: 'numeric',
+      minute: '2-digit',
+    };
+    const d = new Date(Date.parse(card.displayTime.$date));
+    const displayDate = d.toLocaleString('en-US', dateOptions);
+    const displayTime = d.toLocaleString('en-US', timeOptions);
     return (
       <li key={card._id.$oid}>
-        <time className='cbp_tmtime' dateTime='2013-04-10 18:30'>
-          <span>4/10/13</span> <span>18:30</span>
+        <time className='cbp_tmtime' dateTime={d}>
+          <span>{displayDate}</span> <span>{displayTime}</span>
         </time>
 
         <div className='cbp_tmlabel'>
