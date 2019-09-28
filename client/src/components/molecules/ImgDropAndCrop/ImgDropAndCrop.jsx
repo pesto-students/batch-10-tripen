03b117840df7995c8ce6834869228ecea9d52912
Sarthak Chowdhary <sarthak.chow@gmail.com>
2019-09-28 22:15:53 +0530

    added react-dropzone for image upload
    refactored timeline cover image
    refactored timeline

---
+++ client/src/components/molecules/ImgDropAndCrop/ImgDropAndCrop.jsx
@@ -0,0 +1,23 @@
+/* eslint-disable react/jsx-props-no-spreading */
+import React from 'react';
+import Dropzone from 'react-dropzone';
+
+const ImgDropAndCrop = () => {
+  const handleOnDrop = (files) => (console.log(files));
+  return (
+    <div>
+      <Dropzone onDrop={handleOnDrop} maxSize={3145728}>
+        {({ getRootProps, getInputProps }) => (
+          <section>
+            <div {...getRootProps()}>
+              <input {...getInputProps()} />
+              <p>Drag &apos;n&apos; drop cover photo here. (Max size 3MB)</p>
+            </div>
+          </section>
+        )}
+      </Dropzone>
+    </div>
+  );
+};
+
+export default ImgDropAndCrop;
--- client/src/components/organisms/CoverImage/CoverImage.jsx
+++ client/src/components/organisms/CoverImage/CoverImage.jsx
@@ -1,25 +1,67 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './CoverImage.css';
+import { Button, Form } from 'react-bootstrap';
+import ImgDropAndCrop from '../../molecules/ImgDropAndCrop/ImgDropAndCrop';
 
-const Background =
-  'https://images.unsplash.com/photo-1540959733332-eab4deabeeaf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1542&q=80';
+const Background = 'https://images.unsplash.com/photo-1540959733332-eab4deabeeaf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1542&q=80';
 
 const CoverImage = () => {
+  const [timeline, setTimeline] = useState({
+    bg: Background, title: 'My Trip To Tokyo', tagline: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.', author: 'Ayush Jaiswal',
+  });
+  const {
+    bg, title, tagline, author,
+  } = timeline;
+
+  const [edit, setEdit] = useState(false);
+
+  const handleClick = () => setEdit(!edit);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setEdit(!edit);
+  };
+  const handleChange = (e) => {
+    setTimeline({ ...timeline, [e.target.name]: e.target.value });
+  };
   return (
     <div
-      style={{ backgroundImage: `url(${Background})` }}
+      style={{ backgroundImage: `url(${bg})` }}
       className="cover-image"
     >
       <div className="layer">
         <div className="flex-container">
-          <h1>My Trip To Tokyo</h1>
+          <Button onClick={handleClick}>{edit ? 'Done' : 'Edit'}</Button>
+          <ImgDropAndCrop />
+          {edit ? (
+            <Form onSubmit={handleSubmit}>
+              <Form.Group>
+                <Form.Label hidden>Trip Title</Form.Label>
+                <Form.Control type="text" placeholder="Enter Trip Title..." value={title} onChange={handleChange} name="title" />
+              </Form.Group>
+            </Form>
+          )
+            : <h1>{title}</h1>}
+
+
           <blockquote className="blockquote text-center">
-            <p className="mb-0">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
-              posuere erat a ante.
-            </p>
+            {edit ? (
+              <Form onSubmit={handleSubmit}>
+                <Form.Group>
+                  <Form.Label hidden>Trip Tagline</Form.Label>
+                  <Form.Control type="text" placeholder="Enter Trip Tagline..." value={tagline} onChange={handleChange} name="tagline" />
+                </Form.Group>
+              </Form>
+            )
+              : (
+                <p className="mb-0">
+                  {tagline}
+                </p>
+              )}
+
             <footer className="blockquote-footer">
-              by <cite title="Source Title">Andrew Jaiswal</cite>
+              by
+              {' '}
+              <cite title="Source Title">{author}</cite>
             </footer>
           </blockquote>
         </div>
