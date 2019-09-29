503cb553849523be127ce590c29ff2e6ee4d37a8
Sarthak Chowdhary <sarthak.chow@gmail.com>
2019-09-30 00:22:54 +0530

    added react dropzone

--- client/src/components/molecules/ImgDropAndCrop/ImgDropAndCrop.jsx
+++ client/src/components/molecules/ImgDropAndCrop/ImgDropAndCrop.jsx
@@ -1,23 +1,59 @@
 /* eslint-disable react/jsx-props-no-spreading */
-import React from 'react';
-import Dropzone from 'react-dropzone';
+import React, { useState } from 'react';
+import { useDropzone } from 'react-dropzone';
+import { Button } from 'react-bootstrap';
+
 
 const ImgDropAndCrop = () => {
-  const handleOnDrop = (files) => (console.log(files));
+  const [imgSrc, setImgSrc] = useState(null);
+
+  const maxSize = 3145728;
+
+  const handleOnDrop = (file, rejectedFile) => {
+    if (file && file.length > 0) {
+      const currentFile = file[0];
+      const reader = new FileReader();
+      reader.addEventListener('load', () => {
+        setImgSrc(reader.result);
+      }, false);
+      reader.readAsDataURL(currentFile);
+    } else if (rejectedFile && rejectedFile.length > 0) {
+      if (rejectedFile[0].size > 0) {
+        //   Set Alert here
+      }
+    }
+  };
+
+  const { acceptedFiles, getRootProps, getInputProps } = useDropzone({
+    onDrop: handleOnDrop,
+    maxSize,
+    multiple: false,
+    accept: 'image/*',
+  });
+
+  const files = acceptedFiles.map((file) => (`${file.path}-${file.size} bytes`));
+
+  const removeImage = () => {
+    setImgSrc(null);
+  };
+
+
+  const imageDiv = () => (
+    <div>
+      <Button onClick={removeImage}>Remove</Button>
+    </div>
+  );
   return (
     <div>
-      <Dropzone onDrop={handleOnDrop} maxSize={3145728} multiple={false} accept="image/*">
-        {({ getRootProps, getInputProps }) => (
-          <section>
-            <div {...getRootProps()}>
-              <input {...getInputProps()} />
-              <p>Drag &apos;n&apos; drop cover photo here. (Max size 3MB)</p>
-            </div>
-          </section>
-        )}
-      </Dropzone>
+      <div {...getRootProps({ className: 'dropzone text-center' })}>
+        <input {...getInputProps()} />
+        {imgSrc === null ? null : imageDiv()}
+        {files.length > 0 ? <p>{files}</p>
+          : <p>Drag &apos;n&apos; drop cover image here. (Max size 3145728 bytes)</p>}
+      </div>
     </div>
   );
 };
+
 
 export default ImgDropAndCrop;
