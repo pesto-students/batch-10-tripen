7cba28f4e1b5459ae3620d8ff4115dc518615c33
p-racoon <prasham9.ash@gmail.com>
2019-10-03 19:53:02 +0530

    chore: homepage server integration
    fix: #133

--- client/src/containers/Home/fetchTimelines.js
+++ client/src/containers/Home/fetchTimelines.js
@@ -1,34 +1,18 @@
-export default function fetchTimelines(setTimelineData, setIsFetching) {
+import axios from 'axios';
+import serverUrl from '../../serverUrl';
+
+const fetchTimelines = async (setTimelineData, setIsFetching) => {
   setIsFetching(true);
-  const timelineData = [
-    {
-      _id: 'DK22T123',
-      coverImg: 'http://lorempixel.com/180/100/nature/',
-      title: 'title123',
-      tagline: 'tagline123',
-      updatedAt: '2019-10-01T12:50:11Z',
-    },
-    {
-      _id: 'DK22T1234',
-      coverImg: 'http://lorempixel.com/180/100/nature/',
-      title: 'title1234',
-      tagline: 'tagline1234',
-      updatedAt: '2019-10-01T12:50:11Z',
-    },
-    {
-      _id: 'DK22T1235',
-      coverImg: 'http://lorempixel.com/180/100/nature/',
-      title: 'title1235',
-      tagline: 'tagline1235',
-      updatedAt: '2019-10-01T12:50:11Z',
-    },
-    {
-      _id: 'DK22T1236',
-      coverImg: 'http://lorempixel.com/180/100/nature/',
-      title: 'title1236',
-      tagline: 'tagline1236',
-      updatedAt: '2019-10-01T12:50:11Z',
-    },
-  ];
-  setTimelineData(timelineData);
-}
+  const config = {
+    headers: { 'Content-Type': 'application/json' },
+  };
+  try {
+    const res = await axios.get(`${serverUrl}/api/v1/timeline`, config);
+    setTimelineData(res.data.data);
+    setIsFetching(false);
+  } catch (err) {
+    console.warn(err);
+    setIsFetching(false);
+  }
+};
+export default fetchTimelines;
