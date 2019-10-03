0f996d2cda10cc53c472e0f08366062315a18cf4
thapasya <thapasya.murali@gmail.com>
2019-10-04 00:59:59 +0530

    fix:test files changed, timeline delte msg

--- server/src/routes/api/v1/timeline.js
+++ server/src/routes/api/v1/timeline.js
@@ -31,7 +31,10 @@ router
 router
   .route('/:id')
   .get(verifyMongooseId, getTimelineById)
-  .delete(verifyMongooseId, deleteTimeline)
+  .delete(
+    verifyMongooseId,
+    verifyUserPermission,
+    deleteTimeline)
   .put(
     verifyMongooseId,
     verifyTimelineObject,
