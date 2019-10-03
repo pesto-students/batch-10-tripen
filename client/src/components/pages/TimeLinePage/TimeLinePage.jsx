7bb7bd6b9a99b865e73a4b8e61fb8f789953f2a5
Sarthak Chowdhary <sarthak.chow@gmail.com>
2019-10-03 21:04:13 +0530

    fetch timelines

--- client/src/components/pages/TimeLinePage/TimeLinePage.jsx
+++ client/src/components/pages/TimeLinePage/TimeLinePage.jsx
@@ -1,14 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import serverUrl from '../../../serverUrl';
 import CoverImage from '../../organisms/CoverImage/CoverImage';
 import TimeLine from '../../organisms/TimeLine/TimeLine';
 
-const TimeLinePage = () => {
+const TimeLinePage = (props) => {
+  const { timeline_id } = props.match.params;
+
+  const [timelineData, setTimelineData] = useState({});
+
+  const getTimelineData = async () => {
+    console.log(props);
+    const res = await axios.get(`${serverUrl}/api/v1/timeline/${timeline_id}`);
+    setTimelineData(res.data.data);
+  };
+
+  useEffect(() => {
+    getTimelineData();
+  }, [timeline_id]);
+
   return (
     <>
-      <CoverImage></CoverImage>
-      <TimeLine></TimeLine>
+      <CoverImage bg={timelineData.coverImg} title={timelineData.title} tagline={timelineData.tagline} />
+      <TimeLine posts={timelineData.posts} />
     </>
   );
 };
+
 
 export default TimeLinePage;
